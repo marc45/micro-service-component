abef75077e07b65b89e4ae5b0723653d1e74ef7b
hubin <hubin@yunxiaoyuan.net>
2015-12-29 13:28:03 +0800

    自定义测试 MyToken

---
+++ kisso_jfinal/src/main/java/com/baomidou/kisso/MyToken.java
@@ -0,0 +1,16 @@
+package com.baomidou.kisso;
+
+public class MyToken extends SSOToken {
+	private String abc;
+
+	
+	public String getAbc() {
+		return abc;
+	}
+
+	
+	public void setAbc( String abc ) {
+		this.abc = abc;
+	}
+
+}
