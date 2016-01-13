d0413ffdc45bbbf22fac7d2b2fe7caad2eece01f
hubin <hubin@yunxiaoyuan.net>
2016-01-13 17:11:55 +0800

    测试 kisso 3.5.2

--- kisso_jfinal/src/main/java/com/baomidou/kisso/MyToken.java
+++ kisso_jfinal/src/main/java/com/baomidou/kisso/MyToken.java
@@ -1,5 +1,16 @@
 package com.baomidou.kisso;
 
+/**
+ * 
+ * 
+ * 这个是测试自定义 Token 
+ * <p>
+ * 线上可以直接使用 SSOToken 去掉 sso.properties 中配置 sso.token.class=com.baomidou.kisso.MyToken
+ * </p>
+ * 
+ * @author hubin
+ *
+ */
 public class MyToken extends SSOToken {
 	private String abc;
 
