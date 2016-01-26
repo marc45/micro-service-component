8314fec32677bbd8bacbaa478c288a1499aa56ab
dudan <lendo.du@gmail.com>
2016-01-26 11:16:44 +0800

    加入APP拦截代码

--- kisso_jfinal/src/main/java/com/baomidou/kisso/plugin/SSOJfinalInterceptor.java
+++ kisso_jfinal/src/main/java/com/baomidou/kisso/plugin/SSOJfinalInterceptor.java
@@ -41,37 +41,43 @@ import com.jfinal.aop.Invocation;
  */
 public class SSOJfinalInterceptor extends KissoAbstractInterceptor implements Interceptor {
 
-	private static final Logger logger = Logger.getLogger("SSOJfinalInterceptor");
+  private static final Logger logger = Logger.getLogger("SSOJfinalInterceptor");
 
-
-	public void intercept( Invocation inv ) {
-		/**
-		 * 正常执行
-		 */
-		HttpServletRequest request = inv.getController().getRequest();
-		HttpServletResponse response = inv.getController().getResponse();
-		Token token = SSOHelper.getToken(request);
-		if ( token == null ) {
-			if ( "XMLHttpRequest".equals(request.getHeader("X-Requested-With")) ) {
-				/*
-				 * Handler 处理 AJAX 请求
-				 */
-				getHandlerInterceptor().preTokenIsNullAjax(request, response);
-			} else {
-				try {
-					logger.fine("logout. request url:" + request.getRequestURL());
-					SSOHelper.clearRedirectLogin(request, response);
-				} catch ( IOException e ) {
-					e.printStackTrace();
-				}
-			}
-		} else {
-			/*
-			 * 正常请求，request 设置 token 减少二次解密
-			 */
-			request.setAttribute(SSOConfig.SSO_TOKEN_ATTR, token);
-			inv.invoke();
-		}
-	}
+  public void intercept(Invocation inv) {
+    /**
+     * 正常执行
+     */
+    HttpServletRequest request = inv.getController().getRequest();
+    HttpServletResponse response = inv.getController().getResponse();
+    Token token = SSOHelper.getToken(request);
+    if (token == null) {
+      if ("XMLHttpRequest".equals(request.getHeader("X-Requested-With"))) {
+        /*
+         * Handler 处理 AJAX 请求
+         */
+        getHandlerInterceptor().preTokenIsNullAjax(request, response);
+      } else if ("APP".equals(request.getHeader("PLATFORM"))) {
+        /*
+         * Handler 处理 APP接口调用 请求
+         * 没有修改kisso核心代码，直接使用Ajax的认证判断方式，如果未认证，返回401状态码
+         */
+        getHandlerInterceptor().preTokenIsNullAjax(request, response);
+        logger.info("request from APP invoke");
+      } else {
+        try {
+          logger.fine("logout. request url:" + request.getRequestURL());
+          SSOHelper.clearRedirectLogin(request, response);
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    } else {
+      /*
+       * 正常请求，request 设置 token 减少二次解密
+       */
+      request.setAttribute(SSOConfig.SSO_TOKEN_ATTR, token);
+      inv.invoke();
+    }
+  }
 
 }
